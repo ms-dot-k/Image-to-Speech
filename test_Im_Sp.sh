4e5d50c4d5154438f64a427d22aa0ba2c1fc98f8
Minsu Kim <92974900+ms-dot-k@users.noreply.github.com>
2023-10-05 12:02:24 -0400

    Update test_Im_Sp.sh

--- test_Im_Sp.sh
+++ test_Im_Sp.sh
@@ -6,7 +6,7 @@ TRANS=${ROOT}/transcription
 DEVICE_ID=0
 
 REF=data_to/COCO_2014
-CKPT=./pretrained/IM_to_SP.ckpt
+CKPT=./pretrained/Im_to_Speech_Pretrained.ckpt
 Fairseq_path=path_to/fairseq
 
 python test_Im_Sp.py \
